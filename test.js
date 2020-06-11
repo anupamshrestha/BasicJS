c7cafc35510cfa3e2a206d62a74b22c52bded76f
Anupam Shrestha <anupam.stha@gmail.com>
2020-06-11 15:17:56 +1000

    incomplete

---
+++ test.js
@@ -0,0 +1,8 @@
+require("./userClass.js");
+
+ let user = new UserClass('anupam shrestha','Software EnginEering', 'anupam@gmail.com');
+console.log(user.cleanUserData());
+
+
+
+// console.log(valid.properCase());
